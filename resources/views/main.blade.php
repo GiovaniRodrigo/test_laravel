7d17598e9be783c83b53dcb013ac15a448112ed4
GiovaniRodrigo <109702583+GiovaniRodrigo@users.noreply.github.com>
2023-04-17 03:08:47 -0300

    novo layout abastecer
    Signed-off-by: GiovaniRodrigo <109702583+GiovaniRodrigo@users.noreply.github.com>

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,6 +1,6 @@
 
 <html>
-<body>
+<body style="font-family: sans-serif">
     <header style="background-color:rgb(7, 47, 82); position:relative;top:-8px;left:-8px;width:110%">
         <h1 style="font-family: sans-serif;
         text-align:center;
@@ -13,116 +13,146 @@
     </header>
     <section style="width: 50%; position:relative;
                     left:25%;
-                    display:flex;
+                    margin-bottom:20px;
                     ">
 
 
 
-            <button style="width:100%;height:100%;" onclick="show()">Abastecer</button>
 
-            <button style="width:100%;height:100%;" onclick="showRodar()">rodar</button>
-
-            <button style="width:100%;height:100%;" onclick="showContar()">contar</button>
-
-            <a href="carros/create"><button style="width:100%;height:100%;">cadastro</button></a>
+            <a href="carros/create" style="position: relative; left:100%;"><button style="">Cadastrar novo</button></a>
 
 
 
     </section>
-
-    <section style="position: relative; left:15%;">
-        <form method="post" action="/main/abastecer/{{ $carros }}">
-            @csrf
-            @method('PUT')
-            <div style="height:100px"></div>
-            <div id="abastecer" style="display:none">
-                <label for="">Insira a quantia a ser inserida no carro: </label>
-                <input type="number">
-            </div>
-            <div id="rodar" style="display:none">
-                <label for="">Insira a quantia de litros utiliizados: </label>
-                <input type="number">
-            </div>
-            <div id="contar" style="display:none">
-                Mostrar o numero de litrso disponiveis
-            </div>
-
-            </div>
-            <button style="display: none;" id="enviar">Enviar</button>
-            <button style="display:none;" id="cancelar" onclick="cancelar()">Cancelar</button>
-        </form>
-    </section>
-    <div>
-        @isset($carros)
-        @foreach($carros as $carro)
-            <p style="background-color:tomato;">
-                <tr>
-                    <td scropt="row">{{ $loop->index + 1 }}</td>
-                    -
-                    <td >{{ $carro->capacidade }}</td>
-                    -
-                    <td >{{ $carro->portador }}</td>
-                    <td>
-                        <a href="/carros/edit/{{ $carro->id }}" class="btn btn-info edit-btn"> <ion-icon name="create-outline"></ion-icon> Editar</a>
-                        <form action="/carros/{{ $carro->id }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger delete-btn"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
-                        </form>
-                    </td>
-                </tr>
-            </p>
-        @endforeach
-    @endisset
+    <div style="display:flex;gap:20px;position:relative; left:10px;">
+        <div style="position:relative; left:10px;">
+            id
+        </div>
+        <div style="position:relative; left:10px;">
+            Qnt. <br>Inserida
+        </div>
+        <div>
+            Portador
+        </div>
+        <div style="position:relative; left:100px;">
+            Abastecer
+        </div>
+        <div style="position:relative; left:300px;">
+            Saldo do tanque
+        </div>
     </div>
+
+
+    @isset($carros)
+    @foreach($carros as $carro)
+    <div style="display: flex;width:100%;background-color:tomato;">
+        <p >
+            <tr style="border-color:black;">
+                <td scropt="row" > {{ $loop->index + 1 }}</td>
+                -
+                <td >{{ $carro->capacidade }}</td>
+                -
+                <td >{{ $carro->portador }}</td>
+                <td>
+                    <div style="display:flex;">
+
+
+
+                            <form action="/carros/{{ $carro->id }}" method="POST">
+                                @csrf
+
+                            </form>
+                            <div>
+                               <!-- <button id="btn-abastecer{{$carro->id}}" style="width:100%;height:100%;" onclick="show({{$carro->id}})">Abastecer</button>
+                            </div>
+                            <button style="width:100%;height:100%;" onclick="showRodar()">rodar</button>
+
+                            <button style="width:100%;height:100%;" onclick="showContar()">contar</button>
+                        -->
+
+                        <div style="">
+                            <form method="post" action="/main/abastecer/{{ $carros }}" style="display:flex;">
+                                @csrf
+                                @method('PUT')
+                                <div style="height:100px"></div>
+                                <div id="abastecer{{$carro->id}}" style="display:block; padding:10px;">
+                                    <label for="">Insira a quantia a ser inserida no carro: </label>
+                                    <input type="number" size="2">
+                                </div>
+                                <div id="rodar" style="display:block;margin:5px;">
+                                    <label for="">Insira a quantia de litros utiliizados: </label>
+                                    <input type="number">
+                                </div>
+                                <div>
+                                    <button style="margin:5px"><a href="/carros/edit/{{ $carro->id }}" class="btn btn-info edit-btn" style="text-decoration:none;color:black;"> <ion-icon
+                                name="create-outline"></ion-icon> Editar</a></button>
+                                </div>
+                                <div>
+                                    <button type="submit" style="display: block;margin:5px;" id="enviar">Enviar</button>
+                                </div>
+
+                                <div>
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger delete-btn" style="margin:5px;"><ion-icon name="trash-outline"></ion-icon> Deletar</button>
+                                </div>
+
+                                </div>
+
+                                <div>
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </td>
+
+            </tr>
+        </p>
+    </div>
+    @endforeach
+@endisset
 
 </body>
 <script>
-    function show () {
-        let d = document.getElementById("abastecer").style.display;
+    function show ($receber) {
+        let d = document.getElementById("abastecer"+$receber).style.display;
 
-        if(d!="none"){
-            document.getElementById("abastecer").style.display = "none";
-            document.getElementById("enviar").style.display = "none";
-            document.getElementById("cancelar").style.display = "none";
+        if(d!="block"){
+            document.getElementById("abastecer"+$receber).style.display = "block";
+            document.getElementById("btn-abastecer"+$receber).style.display = "none";
         }else{
-            document.getElementById("abastecer").style.display = "block";
-            document.getElementById("rodar").style.display = "none";
-            document.getElementById("contar").style.display = "none";
-            document.getElementById("enviar").style.display = "block";
+            document.getElementById("abastecer"+$receber).style.display = "none";
+            document.getElementById("btn-abastecer"+$receber).style.display = "none";
         }
     }
     function showRodar () {
         let d = document.getElementById("rodar").style.display;
 
-        if(d!="none"){
-            document.getElementById("rodar").style.display = "none";
-            document.getElementById("enviar").style.display = "none";
+        if(d!="block"){
+            document.getElementById("rodar").style.display = "block";
+            document.getElementById("enviar").style.display = "block";
         }else{
             document.getElementById("rodar").style.display = "block";
-            document.getElementById("abastecer").style.display = "none";
-            document.getElementById("contar").style.display = "none";
+            document.getElementById("abastecer").style.display = "block";
+            document.getElementById("contar").style.display = "block";
             document.getElementById("enviar").style.display = "block";
         }
     }
     function showContar () {
         let d = document.getElementById("contar").style.display;
 
-        if(d!="none"){
-            document.getElementById("contar").style.display = "none";
-            document.getElementById("enviar").style.display = "none";
+        if(d!="block"){
+            document.getElementById("contar").style.display = "block";
+            document.getElementById("enviar").style.display = "block";
         }else{
             document.getElementById("contar").style.display = "block";
-            document.getElementById("rodar").style.display = "none";
-            document.getElementById("abastecer").style.display = "none";
-            document.getElementById("enviar").style.display = "none";
+            document.getElementById("rodar").style.display = "block";
+            document.getElementById("abastecer").style.display = "block";
+            document.getElementById("enviar").style.display = "block";
         }
     }
     function cancelar () {
-        document.getElementById("abastecer").style.display = "none";
-        document.getElementById("rodar").style.display = "none";
-        document.getElementById("contar").style.display = "none";
-        document.getElementById("enviar").style.display = "none";
+        document.getElementById("btn-abastecer").style.display = "block";
     }
 
 </script>
